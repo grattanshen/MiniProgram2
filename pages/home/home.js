2fa65f3f794bcbb00fe4b5e618c155faf85e46ba
unknown <unknown@example.com>
2020-04-15 21:45:40 +0800

    4.15V2

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,21 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
+  data:{
+    name : 'Hello World',
+    age : 18,
+    students : [
+      {id:'001', name:'张三', age:23},
+      {id:'002', name:'李四', age:25},
+      {id:'005', name:'王五', age:28}
+    ],
+    counter : 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  handleBtnClick(){
+    // console.log('点击.');
+    // this.data.counter++;
+    // console.log(this.data.counter);
+    this.setData({
+      counter : this.data.counter + 1
+    })
   }
 })
